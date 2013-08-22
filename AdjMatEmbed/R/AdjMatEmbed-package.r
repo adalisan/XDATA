ebc4907282544f4b5bebec9c8eb00fbc63393780
Sancar Adali <sadali1@jhu.edu>
2013-08-21 22:27:27 -0400

    synchronize with gitxdata repo

--- AdjMatEmbed/R/AdjMatEmbed-package.r
+++ AdjMatEmbed/R/AdjMatEmbed-package.r
@@ -2,4 +2,18 @@
 #'
 #' @name AdjMatEmbed
 #' @docType package
+#' @title The AdjMatEmbed Package
+#' @author Sancar Adali \email{sadali1@jhu.edu}
+#' @keywords package
+#'
+#' \tabular{ll}{
+#' Package: \tab AdjMatEmbed\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.5\cr
+#' Date: \tab 2013-08-13\cr
+#' Depends: \tab igraph, reshape2, animation, gridExtra, ggplot2, MCMCpack, mclust, Matrix, irlba, plyr, maps, R.utils, cluster, mapproj, R.utils, chron\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
 NULL
