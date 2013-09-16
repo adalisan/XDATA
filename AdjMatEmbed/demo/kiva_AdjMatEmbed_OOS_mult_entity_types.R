ed3a5e2648347d2a4e306005feea0e8a496b2447
Sancar Adali <sadali1@jhu.edu>
2013-09-16 19:08:40 -0400

    sync with gitxdata repo version of AdjMatEmbed

--- AdjMatEmbed/demo/kiva_AdjMatEmbed_OOS_mult_entity_types.R
+++ AdjMatEmbed/demo/kiva_AdjMatEmbed_OOS_mult_entity_types.R
@@ -14,11 +14,19 @@ names(kiva.lender_partner.edgelist)<-c("lender","partner")
 
 embed.dim<-10
 lender.embed.all<- lender.embed.all[,-which(colnames(lender.embed.all)=="v.name")]
-
+lender.embed.all <- as.matrix(lender.embed.all)
 
 Partner.Embed <- EmbedOOS (lender.embed.all, kiva.lender_partner.edgelist)
 
 
+partner.embed.pca <- prcomp(Partner.Embed$X.oos)
+DataF.2<- data.frame(PC1=partner.embed.pca[,1] 
+                    , PC2=partner.embed.pca[,2] 
+                     , PC3=partner.embed.pca[,3] )
+p2 <- ggplot() + geom_point(data=DataF.2,aes(x=PC1,y=PC2))
+ggsave(p2,filename="lender.partner.embed.pdf")
+                             
+                             
 
 
 kiva.all.table <- load.kiva.all.table(kiva.data.loc)
